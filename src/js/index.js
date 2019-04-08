dc8fe3b939c10da8c92344507791198a64a7883b
iwakabe <iwakabe@iwakabenoMacBook-Pro.local>
2019-04-08 15:27:16 +0800

    initial

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$('.ctaslider-wrapper').slick({
+  		infinite: true,
+  		slidesToShow: 3,
+ 		slidesToScroll: 3,
+ 		prevArrow: ".cta-slider-next",
+ 		nextArrow: ".cta-slider-prev"
+	});
+});
+$(document).ready(function(){
+	$('.foodslider-wrapper').slick({
+  		infinite: true,
+  		slidesToShow: 3,
+ 		slidesToScroll: 3,
+ 		prevArrow: ".cta-slider-next",
+ 		nextArrow: ".cta-slider-prev"
+	});
+});
+
+
+
+console.log("testing");
